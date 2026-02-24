09e78d3fa534a6a28bbd38424a25e53e5f62bc8c
Greg Livingston <123greglivin@gmail.com>
2026-02-23 22:15:22 -0600

    Heartbeat: add timestamp

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 import time
+from datetime import datetime
 
 def run():
     print("Buddy Alive System Starting...")
     while True:
-        print("Buddy is running...")
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{now}] Buddy is running...")
         time.sleep(5)
 
 if __name__ == "__main__":
