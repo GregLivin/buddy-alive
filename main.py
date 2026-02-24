0412ec10ff50798a3c5b00dc61e422bf1a1117e6
Greg Livingston <123greglivin@gmail.com>
2026-02-23 21:47:18 -0600

    Add heartbeat loop

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+
+def run():
+    print("Buddy Alive System Starting...")
+    while True:
+        print("Buddy is running...")
+        time.sleep(5)
+
+if __name__ == "__main__":
+    run()
